a523a3a3de5d11e893869233f97ce936023108d6
krishnakumarsingh <krishnakumarsingh@users.noreply.github.com>
2020-04-13 00:09:48 +0530

    Update matrix-spiral.js

--- matrix-spiral.js
+++ matrix-spiral.js
@@ -10,21 +10,25 @@ let eRow = n - 1;
 let sCol = 0;
 let eCol = n - 1;
 while (sCol <= eCol && sRow <= eRow) {
+    // top row
     for (let i = sCol; i <= eCol; i++) {
         results[sRow][i] = counter;
         counter++;
     }
     sRow++;
+    // right column
     for (let i = sRow; i <= eRow; i++) {
         results[i][eCol] = counter;
         counter++;
     }
     eCol--;
+    // bottom row
     for (let i = eCol; i >= sCol; i--) {
         results[eRow][i] = counter;
         counter++;
     }
     eRow--;
+    // start column
     for (let i = eRow; i >= sRow; i--) {
         results[i][sCol] = counter;
         counter++;
